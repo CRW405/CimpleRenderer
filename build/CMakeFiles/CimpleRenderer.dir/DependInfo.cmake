9f54a505c7ea62cfecb49ea816648a5f943dc424
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-08-14 22:08:42 -0500

    got a basic obj viewer up and running

---
+++ build/CMakeFiles/CimpleRenderer.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/cachy/Documents/CimpleRenderer/src/draw.c" "CMakeFiles/CimpleRenderer.dir/src/draw.c.o" "gcc" "CMakeFiles/CimpleRenderer.dir/src/draw.c.o.d"
+  "/home/cachy/Documents/CimpleRenderer/src/input.c" "CMakeFiles/CimpleRenderer.dir/src/input.c.o" "gcc" "CMakeFiles/CimpleRenderer.dir/src/input.c.o.d"
+  "/home/cachy/Documents/CimpleRenderer/src/main.c" "CMakeFiles/CimpleRenderer.dir/src/main.c.o" "gcc" "CMakeFiles/CimpleRenderer.dir/src/main.c.o.d"
+  "/home/cachy/Documents/CimpleRenderer/src/obj.c" "CMakeFiles/CimpleRenderer.dir/src/obj.c.o" "gcc" "CMakeFiles/CimpleRenderer.dir/src/obj.c.o.d"
+  "/home/cachy/Documents/CimpleRenderer/src/render.c" "CMakeFiles/CimpleRenderer.dir/src/render.c.o" "gcc" "CMakeFiles/CimpleRenderer.dir/src/render.c.o.d"
+  "/home/cachy/Documents/CimpleRenderer/src/term_ops.c" "CMakeFiles/CimpleRenderer.dir/src/term_ops.c.o" "gcc" "CMakeFiles/CimpleRenderer.dir/src/term_ops.c.o.d"
+  "" "CimpleRenderer" "gcc" "CMakeFiles/CimpleRenderer.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/CimpleRenderer.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CimpleRenderer.dir/link.d"
+  "CMakeFiles/CimpleRenderer.dir/src/draw.c.o"
+  "CMakeFiles/CimpleRenderer.dir/src/draw.c.o.d"
+  "CMakeFiles/CimpleRenderer.dir/src/input.c.o"
+  "CMakeFiles/CimpleRenderer.dir/src/input.c.o.d"
+  "CMakeFiles/CimpleRenderer.dir/src/main.c.o"
+  "CMakeFiles/CimpleRenderer.dir/src/main.c.o.d"
+  "CMakeFiles/CimpleRenderer.dir/src/obj.c.o"
+  "CMakeFiles/CimpleRenderer.dir/src/obj.c.o.d"
+  "CMakeFiles/CimpleRenderer.dir/src/render.c.o"
+  "CMakeFiles/CimpleRenderer.dir/src/render.c.o.d"
+  "CMakeFiles/CimpleRenderer.dir/src/term_ops.c.o"
+  "CMakeFiles/CimpleRenderer.dir/src/term_ops.c.o.d"
+  "CimpleRenderer"
+  "CimpleRenderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CimpleRenderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
